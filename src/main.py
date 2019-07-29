daa22ba8c6456bcb58332749edc408b0036018d4
1806exe <1806exe@gmail.com>
2019-07-29 22:10:57 +0300

    add ajira portal website

--- src/main.py
+++ src/main.py
@@ -3,38 +3,54 @@
     Email: 1806exe@gmail.com
     GitHub: https://www.github.com/1806exe
 """
-
 import os
 import time
 import tldextract
 import requests
 from bs4 import BeautifulSoup as Soup
 
+
+# for space between
+def space_btn(n):
+    for i in range(n):
+        print(" ")
+
+
 def create_dirs():
     t = time.localtime()
-    timestamp = time.strftime('%b-%Y', t)
-    path = r'..\data\saved\{}'.format(timestamp)
+    datestamp = time.strftime('%b-%Y', t)
+    timestamp = time.strftime('%H%M')
+    path = r'..\data\saved\{}'.format(datestamp)
     if not os.path.exists(path):
         os.makedirs(path)
 
 
-def setup(url):
+def extract_domain(url):
     ext = tldextract.extract(url)
-    print("Collecting data from {0} Website.......".format(ext.domain))
+    domain = ext.domain
+    return domain
 
-def kijiweni():
-    print('Collecting data...')
-    url = 'http://kijiwe.co.tz'
+
+def setup(url):
+    space_btn(3)
+    print("Collecting data from {0} Website.......".format(extract_domain(url)))
     r = requests.get(url)
     html_contents = r.text
     html_soup = Soup(html_contents, 'html.parser')
+    return html_soup
+
+
+def kijiweni():
+    # call setUp function
+    url = 'http://kijiwe.co.tz'
+    html = setup(url)
     _time = time.localtime()
-    timestamp = time.strftime('%d-%m-%Y %H-%M', _time)
+    timestamp = time.strftime('%d-%m-%Y %H%M', _time)
     dir_name = time.strftime('%b-%Y', _time)
-    file_txt = open('../data/saved/{}/{}.txt '.format(dir_name, name) + timestamp, 'w')
+    file_txt = open('../data/saved/{}/{}.txt '.format(dir_name, extract_domain(url).upper()) + timestamp, 'w')
     tag = '******** JOB AVAILABLE TODAY {} *************** \n'.format(timestamp)
     file_txt.write(tag)
-    for found in html_soup.find_all('div', {'class': 'post'}):
+    for found in html.find_all('div', {'class': 'post'}):
         title = found.h2.text.upper()
         file_txt.write(title)
         file_txt.write('SOURCE LINK -> {}'.format(found.h2.a['href'] + '\n'))
@@ -44,19 +60,30 @@ def kijiweni():
     print(' ')
     print('Finish to collect data from {}'.format(url))
     print('Data saved to ', file_txt.name)
+    space_btn(2)
+
 
 def ajira_portal():
     url = "http://portal.ajira.go.tz/index.php/advert"
-    request = requests.get(url)
-    html_context = request.text
-    clean_data = Soup(html_context, "html.parser")
+    html = setup(url)
+    _time = time.localtime()
+    timestamp = time.strftime('%d-%m-%Y %H%M', _time)
+    dir_name = time.strftime('%b-%Y', _time)
+    file_txt = open('../data/saved/{}/{}.txt '.format(dir_name, extract_domain(url).upper()) + timestamp, 'w')
+    tag = '******** JOB AVAILABLE TODAY {} *************** \n'.format(timestamp)
+    file_txt.write(tag)
+    for data in html.find_all("tr", {'class', 'even'}):
+        title = data.h5.text
+        file_txt.write(title)
+        file_txt.write('Source --> {}'.format(data.h5.a['href']))
+        file_txt.write("--" * 40)
+    file_txt.close()
+    print(' ')
+    print('Finish to collect data from {}'.format(url))
+    print('Data saved to ', file_txt.name)
 
-    for data in clean_data.find_all("tr", {'class', 'even'}):
-        print(data.h5.text)
-        print('Source --> {}'.format(data.h5.a['href']))
-        print("--" * 40)
 
 if __name__ == '__main__':
-    #create_dirs()
-    #kijiweni()
-    setup('http://kijiwe.co.tz')
+    create_dirs()
+    kijiweni()
+    ajira_portal()
