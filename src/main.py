3b3fc08cf9d9d8649679858bb999bf2898756758
1806exe <1806exe@gmail.com>
2019-07-29 21:18:13 +0300

    Finish to integrate with network library

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 """
-
     Author: 1806exe
     Email: 1806exe@gmail.com
     GitHub: https://www.github.com/1806exe
-
 """
 
 import os
-import requests
 import time
+import tldextract
+import requests
 from bs4 import BeautifulSoup as Soup
 
 def create_dirs():
@@ -18,21 +17,23 @@ def create_dirs():
     if not os.path.exists(path):
         os.makedirs(path)
 
+
+def setup(url):
+    ext = tldextract.extract(url)
+    print("Collecting data from {0} Website.......".format(ext.domain))
+
 def kijiweni():
     print('Collecting data...')
-    url = 'http://kijiwe.co.tz/'
-
+    url = 'http://kijiwe.co.tz'
     r = requests.get(url)
     html_contents = r.text
-
     html_soup = Soup(html_contents, 'html.parser')
-    t = time.localtime()
-    timestamp = time.strftime('%d-%m-%Y %H:%M', t)
-    dir_name = time.strftime('%b-%Y', t)
-    file_txt = open('../data/saved/{}/kijiweni.txt '.format(dir_name) + timestamp, 'w')
-    tag = '******** ALL THIS DATA ARE COLLECTED FROM KIJIWENI.CO.TZ *************** \n'
+    _time = time.localtime()
+    timestamp = time.strftime('%d-%m-%Y %H-%M', _time)
+    dir_name = time.strftime('%b-%Y', _time)
+    file_txt = open('../data/saved/{}/{}.txt '.format(dir_name, name) + timestamp, 'w')
+    tag = '******** JOB AVAILABLE TODAY {} *************** \n'.format(timestamp)
     file_txt.write(tag)
-
     for found in html_soup.find_all('div', {'class': 'post'}):
         title = found.h2.text.upper()
         file_txt.write(title)
@@ -44,7 +45,18 @@ def kijiweni():
     print('Finish to collect data from {}'.format(url))
     print('Data saved to ', file_txt.name)
 
+def ajira_portal():
+    url = "http://portal.ajira.go.tz/index.php/advert"
+    request = requests.get(url)
+    html_context = request.text
+    clean_data = Soup(html_context, "html.parser")
+
+    for data in clean_data.find_all("tr", {'class', 'even'}):
+        print(data.h5.text)
+        print('Source --> {}'.format(data.h5.a['href']))
+        print("--" * 40)
 
 if __name__ == '__main__':
-    create_dirs()
-    kijiweni()
+    #create_dirs()
+    #kijiweni()
+    setup('http://kijiwe.co.tz')
