43ddd435c974c04e586795aee0eab6722f513083
Inno Mgubhe <1806exe@gmail.com>
2023-09-11 09:17:39 +0300

    Update packages

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
+import os
 from datetime import datetime
 
 
 def create_timestamp_folder_with_data_subfolder():
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    folder_name = f"folder_{timestamp}"
+    folder_name = f"jobs_{timestamp}"
     data_folder = os.path.join(folder_name, "data")
 
     try:
         os.makedirs(data_folder)  
     except FileExistsError:
-        print(f"Folder '{folder_name}' and 'data' subfolder already exist.")
+        print(f"Jobs '{folder_name}' and 'data' subfolder already exist.")
 
 
 if __name__ == "__main__":
