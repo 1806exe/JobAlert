dcd322cdc33d7427f4feff858fc095a7afbd1226
Inno Mgubhe <1806exe@gmail.com>
2023-09-11 10:14:55 +0300

    Update ajirayako scripts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
+"""
+    Author: 1806exe
+    Email: 1806exe@gmail.com
+    GitHub: https://www.github.com/1806exe
+"""
+
+
 import os
 from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
 
 
 def create_timestamp_folder_with_data_subfolder():
@@ -13,6 +22,17 @@ def create_timestamp_folder_with_data_subfolder():
         print(f"Jobs '{folder_name}' and 'data' subfolder already exist.")
 
 
+def scrape_ajirayako():
+    URL = 'https://ajirayako.co.tz/'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find_all(attrs={'class':'nextajax-item'})
+    for element in results:
+        job_link = element.find("a", class_="job-details-link").attrs['href']
+        print(job_link)
+
+
 if __name__ == "__main__":
 
-    create_timestamp_folder_with_data_subfolder()
+    # create_timestamp_folder_with_data_subfolder()
+    scrape_ajirayako()
