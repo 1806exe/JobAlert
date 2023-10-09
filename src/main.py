aa7eb036c9ef3e6cca515a5dbaa4dfb08f1820e2
Inno Mgubhe <1806exe@gmail.com>
2023-10-09 13:37:37 +0300

    Restructure the whole project and add single source

--- src/main.py
+++ src/main.py
@@ -5,46 +5,61 @@
 """
 
 import os
-from datetime import datetime
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
 
+# Configuration dictionary
+config = {
+    'ajirayako': {
+        'url': 'https://ajirayako.co.tz/',
+        'folder_prefix': 'ajirayako',
+    },
+    'mabumbe': {
+        'url': 'https://mabumbe.com/jobs/',
+        'folder_prefix': 'mabumbe',
+    },
+}
 
-def create_timestamp_folder_with_data_subfolder():
+def create_timestamp_folder_with_data_subfolder(folder_prefix):
     timestamp = datetime.now().strftime("%Y%m%d")
-    folder_name = f"jobs_{timestamp}"
-    # data_folder = os.path.join(folder_name, "data")
+    folder_name = f"{folder_prefix}_{timestamp}"
+    data_folder = os.path.join(folder_name, "data")
 
     try:
-        os.makedirs(folder_name)  
+        os.makedirs(data_folder)
     except FileExistsError:
-        print(f"Jobs '{folder_name}' and 'data' subfolder already exist.")
+        print(f"Folder '{data_folder}' already exists.")
 
+def write_job_data_to_file(results, folder_path, folder_prefix):
+    times = datetime.now().strftime("%H%M")
+    file_name = f"{folder_prefix}_{times}.txt"
+    file_path = os.path.join(folder_path, file_name)
 
-def scrape_ajirayako():
-    create_timestamp_folder_with_data_subfolder()
-    URL = 'https://ajirayako.co.tz/'
+    with open(file_path, "a") as file:
+        for element in results:
+            job_a_tag = element.find("h3", class_="loop-item-title").find('a')
+            job_link = job_a_tag['href']
+            job_title = job_a_tag.text.strip().upper()
+            date_tag = element.find("time", class_="entry-date")
+            job_date = date_tag['datetime']
+
+            file.write(f"{job_link}\n{job_title}\n{job_date}\n\n")
+
+def scrape_website(website_key):
+    website_data = config[website_key]
+    create_timestamp_folder_with_data_subfolder(website_data['folder_prefix'])
+    URL = website_data['url']
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    results = soup.find_all(attrs={'class':'nextajax-item'})
-    times = datetime.now().strftime("%H%M") 
-    file_name = f"ajirayako_{times}.txt"
+    results = soup.find_all(attrs={'class': 'loop-item-content'})
     current_directory = os.getcwd()
-    folder_path = os.path.join(current_directory, 'jobs_{0}'.format(datetime.now().strftime("%Y%m%d")))
-    file_path = os.path.join(folder_path, file_name)
-    os.chdir(folder_path)  
-    for element in results:
-        job_link = element.find("a", class_="job-details-link").attrs['href']
-        job_title = element.find("h3", class_="loop-item-title").text
-        job_date = element.find("span", class_="job-date").text 
-        with open(file_path, "a") as file:
-            file.write('{0}\n'.format(job_link.strip()))
-            file.write('{0}\n'.format(job_title.strip().upper()))
-            file.write('{0}\n'.format(job_date.strip()))
-            file.write('\n')    
-
+    folder_prefix = website_data['folder_prefix']
+    folder_path = os.path.join(current_directory, f"{folder_prefix}_{datetime.now().strftime('%Y%m%d')}")
+    os.chdir(folder_path)
+    write_job_data_to_file(results, folder_path, folder_prefix)
 
 if __name__ == "__main__":
+    for website_key in config:
+        scrape_website(website_key)
 
-    # create_timestamp_folder_with_data_subfolder()
-    scrape_ajirayako()
